40f296f3eb5d5be2eccd9ba76903718e4fc9ef30
Win Tran <nguyentn031192@gmail.com>
2020-03-31 14:40:01 +0200

    prototype

---
+++ Proep-Protoypt/CadillacMicroservices/CadillacMicroservices/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CadillacMicroservices
+{
+    public class User
+    {
+        public string UserEmail
+        {
+            get;set;
+        }
+        public string Password
+        {
+            get;set;
+        }
+        public User(string userEmail, string password)
+        {
+            this.UserEmail = userEmail;
+            this.Password = password;
+        }
+    }
+}
