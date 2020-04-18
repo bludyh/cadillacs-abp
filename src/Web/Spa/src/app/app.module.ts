194f3b17388147f0db89b56628ab42c563c6ec32
Dung Luong <dung291196@gmail.com>
2020-04-18 09:42:49 +0200

    created components for main page

--- src/Web/Spa/src/app/app.module.ts
+++ src/Web/Spa/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { SideBarComponent } from './main-page/side-bar/side-bar.component';
+import { MainContentComponent } from './main-page/main-content/main-content.component';
+import { NotificationBarComponent } from './main-page/notification-bar/notification-bar.component';
+import { NavigationBarComponent } from './main-page/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    SideBarComponent,
+    MainContentComponent,
+    NotificationBarComponent,
+    NavigationBarComponent,
   ],
   imports: [
     BrowserModule
