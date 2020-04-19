e1df10630d3ad0c2a0a02a3426072ccda67ca50a
Phat Tran <phat.tran@student.fontys.nl>
2020-04-19 22:07:00 +0200

    Adding classes.

---
+++ src/Services/Course/Course.Api/Models/StudyMaterial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Api.Models
+{
+    public class StudyMaterial
+    {
+        public int Id { get; set; }
+        public Course Course { get; set; }
+        public Class Class { get; set; }
+        public int Semester { get; set; }
+        public int Year { get; set; }
+
+    }
+}
--- src/Services/Course/Course.Api/Models/WeatherForecast.cs
+++ src/Services/Course/Course.Api/Models/WeatherForecast.cs
@@ -1,7 +1,12 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Course.Api.Models {
-    public class WeatherForecast {
+namespace Course.Api.Models
+{
+    public class WeatherForecast
+    {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
