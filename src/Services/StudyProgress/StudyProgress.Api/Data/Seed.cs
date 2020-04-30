e66d5d596c89f414563e95c7825ee60fba95a8ec
Phat Tran <phat.tran@student.fontys.nl>
2020-04-30 15:53:45 +0200

    Finished adding seed data.

---
+++ src/Services/StudyProgress/StudyProgress.Api/Data/Seed.cs
@@ -0,0 +1,132 @@
+﻿using Bogus;
+using Microsoft.Extensions.Hosting;
+using StudyProgress.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyProgress.Api.Data
+{
+    public static class HostExtensions
+    {
+        public static IHost Seed(this IHost host, StudyProgressContext context)
+        {
+            context.Database.EnsureDeleted();
+            if (!context.Database.EnsureCreated()) return host;
+
+            var schools = new Faker<School>()
+                .StrictMode(false)
+                .Rules((f, s) =>
+                    {
+                        s.Name = f.Company.CompanyName();
+                    }
+                )
+                .Generate(3);
+            context.Schools.AddRange(schools);
+
+            var programs = new Faker<Models.Program>()
+                .StrictMode(false)
+                .Rules((f, p) =>
+                    {
+                        p.Name = f.Lorem.Word();
+                        p.Description = f.Lorem.Sentences();
+                        p.TotalCredit = 60;
+                        p.School = f.PickRandom(schools);
+                    }
+                )
+                .Generate(3);
+            context.Programs.AddRange(programs);
+
+            var courses = new Faker<Course>()
+                .StrictMode(false)
+                .Rules((f, c) =>
+                    {
+                        c.Name = f.Lorem.Word();
+                        c.Description = f.Lorem.Sentence();
+                        c.Credit = 3;
+                    }
+                )
+                .Generate(10);
+            context.Courses.AddRange(courses);
+
+            var courseId = 0;
+            var programCourses = new Faker<ProgramCourse>()
+                .StrictMode(false)
+                .Rules((f, pc) =>
+                    {
+                        pc.Program = f.PickRandom(programs);
+                        pc.Course = courses[courseId++];
+                    }
+                )
+                .Generate(10);
+            context.ProgramCourses.AddRange(programCourses);
+
+            var id = 0;
+            var requirements = new Faker<Requirement>()
+                .StrictMode(false)
+                .Rules((f, r) =>
+                     {
+                         r.Course = courses[id++];
+                         r.RequiredCourse = f.PickRandom(courses);
+                     }
+                )
+                .Generate(5);
+            context.Requirements.AddRange(requirements);
+
+            var faker = new Faker();
+
+            var classes = new List<Class>
+            {
+                new Class
+                {
+                    Id = "E-S71",
+                    Semester = 1,
+                    Year = 3,
+                    Course = faker.PickRandom(courses),
+                    StartDate = faker.Date.Past(),
+                    EndDate = faker.Date.Future()
+                },
+                new Class
+                {
+                    Id = "E-S72",
+                    Semester = 1,
+                    Year = 3,
+                    Course = faker.PickRandom(courses),
+                    StartDate = faker.Date.Past(),
+                    EndDate = faker.Date.Future()
+                }
+            };
+            context.Classes.AddRange(classes);
+
+            var students = new Faker<Student>()
+                .StrictMode(false)
+                .Rules((f, s) =>
+                    {
+                        s.Id = f.Random.ReplaceNumbers("#######");
+                        s.FirstName = f.Name.FirstName();
+                        s.LastName = f.Name.LastName();
+                        s.Initials = f.Random.Replace("?.?.");
+                    }
+                )
+                .Generate(15);
+            context.Students.AddRange(students);
+
+            var enrollmentStudentId = 0;
+            var enrollments = new Faker<Enrollment>()
+                .StrictMode(false)
+                .Rules((f, e) =>
+                    {
+                        e.Class = f.PickRandom(classes);
+                        e.Student = students[enrollmentStudentId++];
+                    }
+                )
+                .Generate(15);
+            context.Enrollments.AddRange(enrollments);
+
+            context.SaveChanges();
+
+            return host;
+        }
+    }
+}
--- src/Services/StudyProgress/StudyProgress.Api/Models/Student.cs
+++ src/Services/StudyProgress/StudyProgress.Api/Models/Student.cs
@@ -7,7 +7,7 @@ namespace StudyProgress.Api.Models
 {
     public class Student
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Initials { get; set; }
