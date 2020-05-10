f22b68bdee56fc7c660528bab1fc859856bd7a62
Nguyen, D.T <n.tthanh@student.fontys.nl>
2020-05-10 21:06:17 +0200

    change apigateway to more simple reroutes

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -9,14 +10,23 @@ using Microsoft.Extensions.Logging;
 
 namespace ApiGateway {
     public class Program {
-        public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", true, true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                        .AddJsonFile("ocelot.json", false, false)
+                        .AddEnvironmentVariables();
+                })
+            .UseStartup<Startup>()
+            .Build();
     }
 }
