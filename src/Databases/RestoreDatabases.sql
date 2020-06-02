23bf6dee8b3fddf1af8e1a4097e2ded98332bc50
Nguyen, D.T <n.tthanh@student.fontys.nl>
2020-06-02 23:29:48 +0200

    add docker and compose support for course api

--- src/Databases/RestoreDatabases.sql
+++ src/Databases/RestoreDatabases.sql
@@ -1,4 +1,5 @@
-RESTORE DATABASE Announcement FROM DISK = '/var/opt/mssql/backup/Announcement.bak' WITH MOVE 'Announcement' TO '/var/opt/mssql/data/Announcement.mdf', MOVE 'Announcement_log' TO '/var/opt/mssql/data/Announcement_log.ldf';
-RESTORE DATABASE [Identity] FROM DISK = '/var/opt/mssql/backup/Identity.bak' WITH MOVE 'Identity' TO '/var/opt/mssql/data/Identity.mdf', MOVE 'Identity_log' TO '/var/opt/mssql/data/Identity_log.ldf';
-RESTORE DATABASE Schedule FROM DISK = '/var/opt/mssql/backup/Schedule.bak' WITH MOVE 'Schedule' TO '/var/opt/mssql/data/Schedule.mdf', MOVE 'Schedule_log' TO '/var/opt/mssql/data/Schedule_log.ldf';
-RESTORE DATABASE StudyProgress FROM DISK = '/var/opt/mssql/backup/StudyProgress.bak' WITH MOVE 'StudyProgress' TO '/var/opt/mssql/data/StudyProgress.mdf', MOVE 'StudyProgress_log' TO '/var/opt/mssql/data/StudyProgress_log.ldf';
+RESTORE DATABASE Announcement FROM DISK = '/var/opt/mssql/backup/Announcement.bak' WITH MOVE 'Announcement' TO '/var/opt/mssql/data/Announcement.mdf', MOVE 'Announcement_log' TO '/var/opt/mssql/data/Announcement_log.ldf', REPLACE;
+RESTORE DATABASE Course FROM DISK = '/var/opt/mssql/backup/Course.bak' WITH MOVE 'Course' TO '/var/opt/mssql/data/Course.mdf', MOVE 'Course_log' TO '/var/opt/mssql/data/Course_log.ldf', REPLACE;
+RESTORE DATABASE [Identity] FROM DISK = '/var/opt/mssql/backup/Identity.bak' WITH MOVE 'Identity' TO '/var/opt/mssql/data/Identity.mdf', MOVE 'Identity_log' TO '/var/opt/mssql/data/Identity_log.ldf', REPLACE;
+RESTORE DATABASE Schedule FROM DISK = '/var/opt/mssql/backup/Schedule.bak' WITH MOVE 'Schedule' TO '/var/opt/mssql/data/Schedule.mdf', MOVE 'Schedule_log' TO '/var/opt/mssql/data/Schedule_log.ldf', REPLACE;
+RESTORE DATABASE StudyProgress FROM DISK = '/var/opt/mssql/backup/StudyProgress.bak' WITH MOVE 'StudyProgress' TO '/var/opt/mssql/data/StudyProgress.mdf', MOVE 'StudyProgress_log' TO '/var/opt/mssql/data/StudyProgress_log.ldf', REPLACE;
