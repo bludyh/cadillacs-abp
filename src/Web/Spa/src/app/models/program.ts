70ae1007c03de4e517f10001056ad4dcc0c862c4
Tran,Nguyen N.N <n.tran@student.fontys.nl>
2020-06-23 14:56:51 +0200

    50% get of course domain

--- src/Web/Spa/src/app/models/program.ts
+++ src/Web/Spa/src/app/models/program.ts
@@ -3,12 +3,6 @@ import { School } from './school';
 export class Program{
     id:string;
     name:string;
-    school:School;
-}
-
-export class DetailProgram{
-    id:string;
-    name:string;
     description:string;
     totalCredit:number;
     school:School;
---
+++ src/Web/Spa/src/app/models/studyMaterial.ts
@@ -0,0 +1,9 @@
+import { Class } from './class';
+
+export class StudyMaterial{
+    id:number;
+    class:Class;
+    name:string;
+    description:string;
+    week:string;
+}
